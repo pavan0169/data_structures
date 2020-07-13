0c7cb3b1b8fd7ec7e70472117bb65a0457005c45
pavan0169 <47805703+pavan0169@users.noreply.github.com>
2020-07-13 23:55:00 +0530

    Update Main.java

--- sortings/Main.java
+++ sortings/Main.java
@@ -5,7 +5,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		int[] arr= {0,12,67,100,200,23,98,65,23,6,44,61,1,400};
-		Insertion.sort(arr);
+		//Insertion.sort(arr); //Selection.sort(arr); //Merge.sort(arr); // Bubble.sort(arr); // Quick.sort(arr); //Heap.sort(arr);
 		for(int i=0; i<arr.length; i++)
 		{
 			System.out.print(arr[i]+" ");
